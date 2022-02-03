b1c414e7a903d506d345873c7561742599b191e7
Tina Thompson <mark_junior13@yahoo.com>
2022-02-03 09:11:50 -0600

    add User-Post association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ApplicationRecord
+  validates :title, presence: true, length: {in: 10..300}
+  validates :body, allow_blank: true, length: {in: 0..20000}
+  
   belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :username, presence: true, uniqueness: true, length: {in: 1..32}
   validates :password, presence: true
+
+  has_many :posts
 end
